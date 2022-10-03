2fd7e361b2da2bcee7215ee6e605a8169e6c78c0
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-03 19:39:26 -0300

    setup basic movie controller

---
+++ app/controllers/MovieController.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const controller = express.Router()
+
+controller.post('/', (req, res) => {
+
+})
+
+controller.get('/', (req, res) => {
+    
+})
+
+module.exports = controller
