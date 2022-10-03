1a1e395164e18fb808a6bd5375193f5f2ebe9721
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-03 19:46:05 -0300

    Setup basic character controller

---
+++ app/controllers/CharacterController.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const controller = express.Router()
+
+controller.post('/', (req, res) => {
+
+})
+
+controller.get('/', (req, res) => {
+    
+})
+
+controller.get('/:id', (req, res) => {
+    
+})
+
+controller.patch('/:id', (req, res) => {
+    
+})
+
+controller.update('/:id', (req, res) => {
+    
+})
+
+controller.delete('/:id', (req, res) => {
+    
+})
+
+module.exports = controller
