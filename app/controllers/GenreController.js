a8792bd1bdb86679b8853d0e3ed98e0f4e97a2c5
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-03 19:43:37 -0300

    Setup basic genre controller

---
+++ app/controllers/GenreController.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const controller = express.Router()
+
+controller.post('/', (req, res) => {
+
+})
+
+controller.get('/', (req, res) => {
+    
+})
+
+controller.get('/:id', (req, res) => {
+    
+})
+
+controller.patch('/:id', (req, res) => {
+    
+})
+
+controller.update('/:id', (req, res) => {
+    
+})
+
+module.exports = controller
